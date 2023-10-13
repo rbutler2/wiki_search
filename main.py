e4aff1c3412e1fe98fe29968066704244805c022
Will Butler <rwbutler2@gmail.com>
2023-10-13 10:48:16 -0500

    more dev on main and gitignore added

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-# import dependancies
-from bs4 import BeautifulSoup as bs
-import requests
-import re
-
-
-
 # ask the user what they would liek to know
 topic = input("Hello, what would you like me to tell you about? ")
 
-print(f"Okay, I will search for {topic}")
+print(f"Okay, I will search for information about {topic}")
+
+# import dependancies
+# note that this is done after the propt so that the user does not experiance a wait time before giving the propt
+from bs4 import BeautifulSoup as bs
+import requests
+from transformers import AutoTokenizer, DataCollatorForSeq2Seq
+from transformers.utils.dummy_pt_objects import TopKLogitsWarper
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import WebBaseLoader
+from langchain.chains.summarize import load_summarize_chain
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+import openai
 
 #save topic as a url
 url = (f'https://en.wikipedia.org/wiki/{topic}')
@@ -17,19 +24,45 @@ url = (f'https://en.wikipedia.org/wiki/{topic}')
 response = requests.get(url)
 
 soup = bs(response.text, 'lxml').text
-#div = soup.find('div', class_='mw-parser-output')
-#p = div.find('p')
 
-#content = soup.find('main', {'id': 'content'}).find_all('p')
+#import api key
+keys_file = open("config.txt")
+lines = keys_file.readlines()
+api_key = lines[0].rstrip()
+openai.api_key = api_key
 
-text = []
-#CLEANR = re.compile('<.*?>') 
+#define the propt
+propt_template = """please summerize the following: "{soup}" CONCISE SUMMARY:"""
 
-#for p in content:
-    #clean_p = re.sub(CLEANR,'', p)
-    #text.append(p)
+propt = PromptTemplate.from_template(propt_template)
+
+#define llm chain
+llm = ChatOpenAI(tempature=0, model_name="gpt-3.5-turbo-16k")
+llm_chain = LLMChain(llm=llm, propt=propt)
+
+#define stuff documant chain
+stuff_chain = StuffDocumentsChain(
+    llm_chain=llm_chain, document_variable_name="soup"
+)
+
+docs = loader.load()
+print(stuff_chain.run(docs))
+
+# tokenizer = AutoTokenizer.from_pretrained(soup)
+
+# prefix = "summarize: "
+
+# def preprocess_function(input):
+#     inputs = [prefix + doc for doc in input["text"]]
+#     model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
+
+#     labels = tokenizer(text_target=input["summary"], max_length=128, truncation=True)
+
+#     model_inputs["labels"] = labels["input_ids"]
+#     return model_inputs
 
 
-#clean_text = re.sub(r'<.*?>','', joined_text)
+# data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=soup, return_tensor="tf")
 
-print(soup)
+
+# print(tokenizer)
