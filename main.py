e5e67a5c625a69143dab43f88f588bb8dcafdc63
Will Butler <rwbutler2@gmail.com>
2023-10-17 17:24:34 -0500

    changed from langchain to sumy

--- main.py
+++ main.py
@@ -6,18 +6,15 @@ print(f"Okay, I will search for information about {topic}")
 # import dependancies
 # note that this is done after the propt so that the user does not experiance a wait time before giving the propt
 from bs4 import BeautifulSoup as bs
+from langchain.chains import summarize
 import requests
 from transformers import AutoTokenizer, DataCollatorForSeq2Seq
 from transformers.utils.dummy_pt_objects import TopKLogitsWarper
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import TextLoader
-from langchain.chains.summarize import load_summarize_chain
-from langchain.chains.llm import LLMChain
-from langchain.prompts import PromptTemplate
-from langchain.chains.combine_documents.stuff import StuffDocumentsChain
-import openai
-import os
 from dotenv import load_dotenv
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.utils import get_stop_words
 
 #save topic as a url
 url = (f'https://en.wikipedia.org/wiki/{topic}')
@@ -27,49 +24,15 @@ response = requests.get(url)
 
 soup = bs(response.text, 'lxml').text
 
-#import api key
-keys_file = open("config.txt")
-lines = keys_file.readlines()
-api_key = lines[0].rstrip()
-#openai.api_key = api_key
-os.environ["OPENAI_API_KEY"] = api_key
+parser = PlaintextParser.from_string(soup, Tokenizer("english"))
 
-#define the propt
-prompt_template = """please summerize the following: "{soup}" CONCISE SUMMARY:"""
+summarizer = TextRankSummarizer()
 
-prompt = PromptTemplate.from_template(prompt_template)
+summarizer.stop_words = get_stop_words("english")
 
-f = open("prompt.txt", mode="wt", encoding="utf-8")
-f.write(f"please summerize the following: {soup} CONCISE SUMMARY:")
+summary_length = 10
 
-#define llm chain
-llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
-llm_chain = LLMChain(llm=llm, prompt=prompt)
+summary = summarizer(parser.document, summary_length)
 
-#define stuff documant chain
-stuff_chain = StuffDocumentsChain(
-    llm_chain=llm_chain, document_variable_name="soup"
-)
-
-loader = TextLoader('prompt.txt')
-docs = loader.load()
-print(stuff_chain.run(docs))
-
-# tokenizer = AutoTokenizer.from_pretrained(soup)
-
-# prefix = "summarize: "
-
-# def preprocess_function(input):
-#     inputs = [prefix + doc for doc in input["text"]]
-#     model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
-
-#     labels = tokenizer(text_target=input["summary"], max_length=128, truncation=True)
-
-#     model_inputs["labels"] = labels["input_ids"]
-#     return model_inputs
-
-
-# data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=soup, return_tensor="tf")
-
-
-# print(tokenizer)
+for sentence in summary:
+    print(sentence)
