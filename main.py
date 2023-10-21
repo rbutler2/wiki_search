e83e6cfb6046097082f8b2e9bb6ce2a238d70b78
Will Butler <rwbutler2@gmail.com>
2023-10-21 09:08:59 -0500

    changeed main.py to use wiki library

--- main.py
+++ main.py
@@ -3,34 +3,47 @@ topic = input("Hello, what would you like me to tell you about? ")
 
 print(f"Okay, I will search for information about {topic}")
 
+email = input("To grab the information from wikipidia please tell me your email ")
+
 # import dependancies
 # note that this is done after the propt so that the user does not experiance a wait time before giving the propt
 from bs4 import BeautifulSoup as bs
-from langchain.chains import summarize
 import requests
-from transformers import AutoTokenizer, DataCollatorForSeq2Seq
-from transformers.utils.dummy_pt_objects import TopKLogitsWarper
 from dotenv import load_dotenv
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.text_rank import TextRankSummarizer
 from sumy.utils import get_stop_words
+import wikipediaapi
 
 #save topic as a url
-url = (f'https://en.wikipedia.org/wiki/{topic}')
+#url = (f'https://en.wikipedia.org/wiki/{topic}')
 
 #grab the web page
-response = requests.get(url)
+#response = requests.get(url)
 
-soup = bs(response.text, 'lxml').text
+#soup = bs(response.text, 'lxml').text
 
-parser = PlaintextParser.from_string(soup, Tokenizer("english"))
+wiki_wiki = wikipediaapi.Wikipedia(f"MyProjectName ({email})","en")
+
+page = wiki_wiki.page(topic)
+
+text = ()
+
+if page.exists():
+    text = page.text 
+else:
+    print("Sorry but this page does not exist")
+
+#print(text)
+
+parser = PlaintextParser.from_string(text, Tokenizer("english"))
 
 summarizer = TextRankSummarizer()
 
 summarizer.stop_words = get_stop_words("english")
 
-summary_length = 10
+summary_length = 20
 
 summary = summarizer(parser.document, summary_length)
 
